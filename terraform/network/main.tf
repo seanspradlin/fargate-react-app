78c6a657a5c430bd7fb750c70334481987577b53
Sean Spradlin <seanspradlin@gmail.com>
2021-10-10 15:00:07 -0500

    forward to 8080

--- terraform/network/main.tf
+++ terraform/network/main.tf
@@ -113,7 +113,7 @@ resource "aws_route53_record" "www" {
 
 resource "aws_lb_target_group" "group" {
   name        = "tf-fargate-demo-tg"
-  port        = "80"
+  port        = "8080"
   protocol    = "HTTP"
   target_type = "ip"
   vpc_id      = aws_vpc.vpc.id
