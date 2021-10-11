b4cce5468404ce1ee94a4a47c293324f04667031
Sean Spradlin <seanspradlin@gmail.com>
2021-10-11 10:09:36 -0500

    respond to all requests with a 200

--- index.js
+++ index.js
@@ -3,12 +3,8 @@ const express = require('express');
 const PORT = 8080;
 const app = express();
 
-app.get('/healthcheck', (req, res) => {
+app.use((req, res) => {
   res.status(200).json({ status: 'OK' });
-});
-
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello World' });
 });
 
 app.listen(PORT, () => {
