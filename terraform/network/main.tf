0eb807c7255f926ea8ea32e8bf6cc653f9440f08
Sean Spradlin <seanspradlin@gmail.com>
2021-10-10 23:17:35 -0500

    update healthcheck options

--- terraform/network/main.tf
+++ terraform/network/main.tf
@@ -129,8 +129,12 @@ resource "aws_lb_target_group" "group" {
   depends_on = [aws_lb.alb]
 
   health_check {
-    matcher = "200"
-    path    = "/healthcheck"
+    matcher             = "200"
+    path                = "/healthcheck"
+    port                = 8080
+    healthy_threshold   = 6
+    unhealthy_threshold = 2
+    timeout             = 5
   }
 
   lifecycle {
