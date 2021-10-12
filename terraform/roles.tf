ec0a0698d6a703bbeef57adbddaa32c372b55c49
Sean Spradlin <seanspradlin@gmail.com>
2021-10-12 17:45:39 -0500

    switch back to full access

--- terraform/roles.tf
+++ terraform/roles.tf
@@ -43,6 +43,6 @@ resource "aws_iam_role" "ecs_task_execution_role" {
 
 resource "aws_iam_role_policy_attachment" "ecs_tas_exection_role_attachment" {
   role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonECS_FullAccess"
 }
 
